54697396ab477b3422708fda0ebcb3a540296744
Christian Back <cdirt27@gmail.com>
2021-04-18 22:16:03 -0500

    server data to be derived from server/public folders, formatted index.html page to have base mode input placeholders, operator  buttons, and div for operation answer/history

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ const express = require('express')
 const app = express();
 const PORT = 5000; 
 
+// Test and see which port the express server is listening on
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
+
+
+app.use(express.static('server/public'));
+
+
 
