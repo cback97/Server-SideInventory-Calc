c37a75a19a39008d434041ae0ffee11936b6d180
Christian Back <cdirt27@gmail.com>
2021-04-19 04:56:27 -0500

    created switch statement function on new object operators to create solves for client calculator

--- server/public/client.js
+++ server/public/client.js
@@ -2,35 +2,74 @@ console.log('JS is good to go')
 $(document).ready(onReady);
 
 function onReady() {
-    $('#calculateInputs').on('click', objectCalculation)
+    $('#calculateInputs').on('click', objectCapture)
     $('.operatorButton').on('click', getOperatorString)
 
 
 
-}
+} //end onReady
 
+// Create global variable for operator capture
 let operator = '';
 
 function getOperatorString() {
-operator = $(this).attr('value')
-}
-
-
-function objectCalculation() {
- let inputCapture = {
-     input1: $('#num1').val(),
-     operator: operator,
-     input2: $('#num2').val(),
-     
- }   
- return console.log(inputCapture);
-
-
-}
+    operator = $(this).attr('value')
+} // end getOperatorString
 
 //clear input fields
 function clearInputs() {
     $('#num1').val('')
     $('#num2').val('')
+} //end clearInputs
+
+function objectCapture() {
+    let inputCapture = {
+        input1: $('#num1').val(),
+        operator: operator,
+        input2: $('#num2').val(),
+
+    }
+
+    $.ajax({
+            method: 'POST',
+            url: '/calculation',
+            data: inputCapture // this is goes in the request body
+        })
+        .then(function (response) {
+            console.log('Response from server', response);
+            render(response);
+            //this is where GET function goes!
+        })
+        .catch(function (error) {
+            console.log('Error from server', error);
+            alert('Sorry, could not get calculations. Try again later.');
+        })
+
+
+} //end objectCapture
+
+function getInventory() {
+    // ajax method returns back Promise
+    $.ajax({
+            method: 'GET',
+            url: '/inventory',
+        })
+
+        .then(function (response) {
+            console.log('Response from server', response);
+            render(response);
+        })
+        .catch(function (error) {
+            console.log('Error from server', error);
+            alert('Sorry, could not get updated stock. Try again later.');
+        })
+    console.log('After makin server request...');
+}
+
+function render(inventoryArray) {
+    $('#stocklist').empty();
+    for (let listed of inventoryArray) {
+        $('#stocklist').append(`<p>${listed.item}: ${listed.description}</p>`)
+    }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // Build server, and establish port for app to listen on
 const express = require('express')
 const app = express();
-const PORT = 5000; 
+const PORT = 5000;
 
 // Test and see which port the express server is listening on
 app.listen(PORT, () => {
@@ -13,23 +13,45 @@ app.use(express.static('server/public'));
 
 // set up how to read things from the request body
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Build array to hold operation history
 const historyArray = [];
 
-app.get('/calculation', (req,res) => {
+app.get('/calculation', (req, res) => {
 
 })
 
-app.post('/calculation', (req,res) => {
-
+app.post('/calculation', (req, res) => {
+    let newOperation = req.body;
+    // create total property for newOperation
+    newOperation.solve = operations(newOperation)
+    // push newOperation object into historyArray
+    historyArray.push(newOperation)
+//check to m
 })
 
-function operations() {
+function operations(newOperation) {
+    let num1 = newOperation.input1;
+    let num2 = newOperation.input2;
+    switch (newOperation.operator) {
+        case '+':
+            solve = num1 + num2;
+            return solve;
+        case '-':
+            solve = num1 - num2;
+            return solve;
+        case '*':
+            solve = num1 * num2;
+            return solve;
+        case '/':
+            solve = num1 / num2;
+            return solve;
+    }
+} // end swith statement function
 
-}
+app.get('/calculation', (req, res) => {
 
-
-
-
+})
